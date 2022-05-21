e84c264cf857866be88eb9b8d2ad90f4a81e3eb3
Allan Resende <allresende@pop-os.localdomain>
2022-05-21 13:45:14 -0300

    swipe adicionado ao card1

--- front/pages/cards_multiplos/script.js
+++ front/pages/cards_multiplos/script.js
@@ -1,17 +1,68 @@
 const card1 = document.getElementById('card1')
+image = card1.getElementsByClassName('image')[0]
 
 function onDrag({movementX}){
     let getStyle = window.getComputedStyle(card1);
     let left = parseInt(getStyle.left);
+    
     card1.style.left = `${left + movementX}px`
 }
 
-card1.addEventListener("mousedown", ()=>{
-    card1.classList.add("active");
-    card1.addEventListener("mousemove", onDrag)
-})
+function onDragMobile(touch){
+    let touchLocation = touch.targetTouches[0]
+    let getStyle = window.getComputedStyle(card1);
+    let left = parseInt(getStyle.left);
+    let width = parseInt(getStyle.width);
 
+    card1.style.left = `${touchLocation.pageX - width/2}px`
+}
+
+function removeCard(){
+    let i = 0
+    let cards = document.getElementsByClassName('card')
+    for (i in cards){
+        if (cards[i].classList[1] == 'swiped'){
+            cards[i].style.display = 'none'
+        }
+    }
+
+}
+//Holding mouse left button to swipe
+image.addEventListener("mousedown", ()=>{
+    image.classList.add("active");
+    image.addEventListener("mousemove", onDrag)
+})
+//Pressing the touchscreen to swipe
+image.addEventListener("touchstart", ()=>{
+    image.classList.add("active");
+    image.addEventListener("touchmove", onDragMobile)
+})
+//Releasing the mouse left button
 document.addEventListener("mouseup", ()=>{
-    card1.classList.remove("active")
-    card1.removeEventListener("mousemove", onDrag)
+    image.classList.remove("active")
+    image.removeEventListener("mousemove", onDrag)
+})
+//Releasing the touchscreen
+image.addEventListener("touchend", ()=>{
+    let getStyle = window.getComputedStyle(card1);
+    let left = parseInt(getStyle.left);
+
+    image.classList.remove("active");
+    image.removeEventListener("touchmove", onDragMobile)
+
+    if (left >= 45){
+        card1.classList.add("swiped");
+        card1.classList.add("right");
+        card1.style.transitionDuration = '0.8s'
+        setTimeout(removeCard,800)
+    } else if (left <= -40){
+        card1.classList.add("swiped");
+        card1.classList.add("left");
+        card1.style.transitionDuration = '0.8s'
+        setTimeout(removeCard,800)
+    } else{
+        card1.style.transitionDuration = '0.1s'
+        card1.style.left = 0;
+        setTimeout(()=>{card1.style.transitionDuration='0s'},150)
+    }
 })
